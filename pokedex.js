cf421d40c360938edfcaf087a14c52522618d79c
Richard Johns <richard.johns193@gmail.com>
2017-10-29 14:48:47 +1300

    hope this doesnt hide my js file

---
+++ pokedex.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer')
+// const {pokemon} = require('./pokedex') NO. BAD. STOP.
+
+const pokemon = ['bulbasaur', 'charmander', 'mewtwo', 'jolteon', 'exeggutor']
+
+pokemon.forEach(name => getData(name))
+
+async function getData (pokemon) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(`https://www.pokemon.com/us/pokedex/${pokemon}`)
+  await page.waitFor(1000)
+  await page.setViewport({width: 1000, height: 1150})
+  await page.screenshot({path: `./pokedex/${pokemon}.jpg`})
+
+  await browser.close()
+}
