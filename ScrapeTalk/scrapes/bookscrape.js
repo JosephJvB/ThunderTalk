da28960282fb9730fb2468a4419d13c2f097e7e7
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-10-31 10:39:27 +1300

    making md for prop

--- ScrapeTalk/scrapes/bookscrape.js
+++ ScrapeTalk/scrapes/bookscrape.js
@@ -1,31 +1,31 @@
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 let scrape = async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({headless: false})
+  const page = await browser.newPage()
 
-    await page.goto('http://books.toscrape.com/');
+  await page.goto('http://books.toscrape.com/')
 
-    const result = await page.evaluate(() => {
-        let data = [];
-        let elements = document.querySelectorAll('.product_pod');
+  const result = await page.evaluate(() => {
+    let data = []
+    let products = document.querySelectorAll('.product_pod')
 
-        products.forEach(product => {
-        const title = product.querySelector('h3').innerText;
-        const price = product.querySelector('.price_color').innerText;
+    products.forEach(product => {
+      const title = product.querySelector('h3').innerText
+      const price = product.querySelector('.price_color').innerText
 
+      data.push({title, price})
+    })
 
-            data.push({title, price});
-        }
+    return data
+  })
 
-        return data;
-    });
-
-    browser.close();
-    return result;
-};
+  browser.close()
+  return result
+}
 
 scrape().then((value) => {
-    console.log(value);
-});
+  console.log(value)
+  // fs.writeFile to save data to file
+})
--- ScrapeTalk/scrapes/pokedex.js
+++ ScrapeTalk/scrapes/pokedex.js
@@ -1,20 +1,18 @@
 const puppeteer = require('puppeteer')
-// const {pokemon} = require('./pokedex') NO. BAD. STOP.
 
 const pokemon = ['bulbasaur', 'charmander', 'mewtwo', 'jolteon', 'exeggutor']
 
 pokemon.forEach(name => getData(name))
 
-// function start () {
 async function getData (pokemon) {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(`https://www.pokemon.com/us/pokedex/${pokemon}`)
   await page.waitFor(1000)
   await page.setViewport({width: 1000, height: 1150})
-  await page.screenshot({path: `./pokedex/${pokemon}.jpg`})
+  await page.screenshot({path: `./ScrapeTalk/scrapes/pokedex/${pokemon}.jpg`})
 
   await browser.close()
 }
-//   start()
-// }
+
+// api to recieve pokemon names and save those to an array, then loop through the array to print screenshots of the pokemon.
